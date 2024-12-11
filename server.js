9226cc32f999cdae0eabc3d2f7a52b2acfffce42
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 05:48:15 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
-const path = require( 'path' );
+const express = require( 'express' );
 const router = express.Router();
+const path = require( 'path' );
 const app = express();
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
-
-module.exports = router;
-
-const profile = require( './routes/api/profile' );
 app.use( '/api/profile', profile );
 
+module.exports = router;
+const profile = require( './routes/api/profile' );
+
 if ( process.env.NODE_ENV === 'production' ) {
-	// Set a static folder
 	app.use( express.static( 'client/build' ) );
 	app.get( '*', ( req, res ) => res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) ) );
-
 }
 
 const port = process.env.PORT || 5000;
