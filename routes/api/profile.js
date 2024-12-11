cd3280f2be600ce779f13731e43e18d48ec5f61c
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 04:39:59 -0800

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,21 +3,14 @@ const aws = require( 'aws-sdk' );
 const multerS3 = require( 'multer-s3' );
 const multer = require('multer');
 const path = require( 'path' );
-
 const router = express.Router();
 
-/**
- * PROFILE IMAGE STORING STARTS
- */
 const s3 = new aws.S3({
 	accessKeyId: 'xxx',
 	secretAccessKey: 'xxx',
 	Bucket: 'yourbucketname'
 });
 
-/**
- * Single Upload
- */
 const profileImgUpload = multer({
 	storage: multerS3({
 		s3: s3,
@@ -33,31 +26,19 @@ const profileImgUpload = multer({
 	}
 }).single('profileImage');
 
-/**
- * Check File Type
- * @param file
- * @param cb
- * @return {*}
- */
 function checkFileType( file, cb ){
-	// Allowed ext
 	const filetypes = /jpeg|jpg|png|gif/;
-	// Check ext
 	const extname = filetypes.test( path.extname( file.originalname ).toLowerCase());
-	// Check mime
 	const mimetype = filetypes.test( file.mimetype );
 	if( mimetype && extname ){
 		return cb( null, true );
-	} else {
+	} 
+	else 
+	{
 		cb( 'Error: Images Only!' );
 	}
 }
 
-/**
- * @route POST /api/profile/business-img-upload
- * @desc Upload post image
- * @access public
- */
 router.post( '/profile-img-upload', ( req, res ) => {
 	profileImgUpload( req, res, ( error ) => {
 		console.log( 'requestOkokok', req.file );
@@ -66,15 +47,12 @@ router.post( '/profile-img-upload', ( req, res ) => {
 			console.log( 'errors', error );
 			res.json( { error: error } );
 		} else {
-			// If File not found
 			if( req.file === undefined ){
 				console.log( 'Error: No File Selected!' );
 				res.json( 'Error: No File Selected' );
 			} else {
-				// If Success
 				const imageName = req.file.key;
 				const imageLocation = req.file.location;
-// Save the file name into database into profile model
 				res.json( {
 					image: imageName,
 					location: imageLocation
@@ -84,11 +62,6 @@ router.post( '/profile-img-upload', ( req, res ) => {
 	});
 });
 
-/**
- * BUSINESS GALLERY IMAGES
- * MULTIPLE FILE UPLOADS
- */
-// Multiple File Uploads ( max 4 )
 const uploadsBusinessGallery = multer({
 	storage: multerS3({
 		s3: s3,
@@ -103,11 +76,7 @@ const uploadsBusinessGallery = multer({
 		checkFileType( file, cb );
 	}
 }).array( 'galleryImage', 4 );
-/**
- * @route POST /api/profile/multiple-file-upload
- * @desc Upload business Gallery images
- * @access public
- */
+
 router.post('/multiple-file-upload', ( req, res ) => {
 	uploadsBusinessGallery( req, res, ( error ) => {
 		console.log( 'files', req.files );
@@ -115,12 +84,10 @@ router.post('/multiple-file-upload', ( req, res ) => {
 			console.log( 'errors', error );
 			res.json( { error: error } );
 		} else {
-			// If File not found
 			if( req.files === undefined ){
 				console.log( 'Error: No File Selected!' );
 				res.json( 'Error: No File Selected' );
 			} else {
-				// If Success
 				let fileArray = req.files,
 					fileLocation;
 				const galleryImgLocationArray = [];
@@ -129,7 +96,6 @@ router.post('/multiple-file-upload', ( req, res ) => {
 					console.log( 'filenm', fileLocation );
 					galleryImgLocationArray.push( fileLocation )
 				}
-				// Save the file name into database
 				res.json( {
 					filesArray: fileArray,
 					locationArray: galleryImgLocationArray
