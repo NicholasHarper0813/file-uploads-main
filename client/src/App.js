b163248018a3afba555d5ce6f9b36c4903dfc78b
nicholas harper <nicholasharper000@gmail.com>
2024-12-18 01:34:29 -0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "./components/Home";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
-class App extends Component {
-  render() {
+class App extends Component 
+{
+  render() 
+ {
     return (
       <div className="App">
 	      <Router>
